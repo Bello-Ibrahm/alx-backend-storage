f9d290aa4df007d12c368945636675ead8c39651
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-07-15 20:13:11 +0100

    Redo Task 5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
+import requests
 from functools import wraps
 from typing import Callable
 
-store = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def count_url_access(method: Callable) -> Callable:
-    """ Decorator counting how many times
-    a URL is accessed """
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
-        store.incr(f'count:{url}')
-        result = store.get(f'result:{url}')
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        store.set(f'count:{url}', 0)
-        store.setex(f'result:{url}', 10, result)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
 
 
-@count_url_access
+@data_cacher
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
